c34cf4843c33baa829f5b768e78e89ec99b1a154
Jesus Mejia <jesusmejded@gmail.com>
2018-05-04 13:34:30 -0500

    minor changes

--- test.c
+++ test.c
@@ -6,19 +6,27 @@
 void randm(double a, double b, double *x, int D){
     int i;
     for (i = 0; i < D; ++i) {
-        x[i] = a + (int) (rand() / (double) (RAND_MAX) * (b - a));
+        x[i] = a + (double) (rand() / (double) (RAND_MAX) * (b - a));
     }
 }
 
 int main(int argc, char const *argv[])
 {
+    // population size
     int N = 17;
+
+    // upper and lower lever dimension
     int D = 2;
+
+    // number of test function
     int id = 5;
 
     double *x = array(N*D);
     double *y = array(N*D);
+
+    // upper level
     double *F = array(N);
+    // upper lower
     double *f = array(N);
 
     srand(time(NULL));
@@ -26,6 +34,7 @@ int main(int argc, char const *argv[])
     randm(-1, 1, x, N*D);
     randm(-1, 1, y, N*D);
 
+    // evaluate
     blb18_leader_cop(N, D, x, y, F, id);
     blb18_follower_cop(N, D, x, y, f, id);
 
