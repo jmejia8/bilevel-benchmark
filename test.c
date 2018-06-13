4cadd86f3a7b39d796fb14e54f95e519c946af06
Jesús Mejía <jesusmejded@gmail.com>
2018-06-13 04:06:37 -0500

    test added

--- test.c
+++ test.c
@@ -15,8 +15,85 @@ double* array(int D){
     return (double *) malloc(D * sizeof( double ));
 }
 
+int test(){
+    int p=3, q=3, r=2, s=0;
+    int D = p + r;
+
+    double *x = array(D);
+    double *y = array(D);
+    double F[1], Fv = 0;
+
+    int i = 0;
+
+    for (i = 0; i < D; ++i){
+            x[i] = 0; y[i] = 0;
+    }
+
+    SDM1_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("1 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+    
+    SDM3_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("3 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+    SDM4_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("4 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+    SDM6_follower(p, 1, r, 2, x, y, F);
+    if (F[0] != 0) {
+        printf("6 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+    SDM8_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("8 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+    for (i = q; i < D; ++i) y[i] = 1;
+
+    SDM2_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("2 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+    SDM7_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("7 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+
+    for (i = 0; i < D; ++i) y[i] = 0;
+    for (i = 0; i < q; ++i) y[i] = 1;    
+    
+    SDM5_follower(p, q, r, x, y, F);
+    if (F[0] != 0) {
+        printf("5 --> %e\n", F[0]);
+        Fv += F[0];
+    }
+
+    if (Fv != 0){
+        printf("Error\n");
+        exit(0);
+    }
+
+    return 1;
+}
+
 int main(int argc, char const *argv[])
 {
+    test();
     srand(time(NULL));
     int i, id;
  
