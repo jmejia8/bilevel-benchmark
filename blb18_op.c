7c108658676c89b57f95be2a090466fed07da7a9
Jesús Mejía <jesusmejded@gmail.com>
2018-08-17 22:35:49 -0500

    new functions

--- blb18_op.c
+++ blb18_op.c
@@ -337,6 +337,252 @@ void SMD8_follower(int p, int q, int r, double *x, double *y, double *f){
     f[0] = f1 + f2 + f3;
 }
 
+void SMD9_leader(int p, int q, int r, double *x, double *y, double *F, double *G){
+    int i;
+    double a = 1.0, b = 1.0;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double F1 =  sphere(x_u1, p);
+    double F2 = -sphere(x_l1, q);
+    double F3 =  0.0;
+
+    for (i = 0; i < r; ++i) {
+        F3 -= pow(x_u2[i], 2) - pow( x_u2[i] - log( 1.0 + x_l2[i] ), 2);
+    }
+
+    F[0] = F1 + F2 + F3;
+
+    G[0] = (sphere(x_u1, p) + sphere(x_u2, r)) / a;
+    G[0] -= floor( G[0] + 0.5 / b);
+}
+
+void SMD9_follower(int p, int q, int r, double *x, double *y, double *f, double *g){
+    int i;
+    double a = 1.0, b = 1.0;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double f1 = sphere(x_u1, p);
+    double f2 = sphere(x_l1, q);
+    double f3 =  0.0;
+
+    for (i = 0; i < r; ++i) {
+        f3 -= pow( x_u2[i] - log( 1.0 + x_l2[i] ), 2);
+    }
+
+    f[0] = f1 + f2 + f3;
+
+    g[0] = (sphere(x_l1, p) + sphere(x_l2, r)) / a;
+    g[0] -= floor( g[0] + 0.5 / b);
+}
+
+void SMD10_leader(int p, int q, int r, double *x, double *y, double *F, double *G){
+    int i;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double F1 = 0.0;
+    double F2 = sphere(x_l1, q);
+    double F3 = 0.0;
+
+    for (i = 0; i < p; ++i){
+        F1 += pow(x_u1[i] - 2.0, 2);
+    }
+
+    for (i = 0; i < r; ++i) {
+        F3 += pow(x_u2[i] - 2.0, 2) - pow( x_u2[i] - tan( x_l2[i] ), 2);
+    }
+
+    F[0] = F1 + F2 + F3;
+
+    double xu1_3[p];
+    double xu2_3[p];
+
+    for (i = 0; i < p; ++i) { xu1_3[i] = pow(x_u1[i], 3); }
+    for (i = 0; i < r; ++i) { xu2_3[i] = pow(x_u2[i], 3); }
+
+    double sum_xu1_3 = 0.0;
+    for (i = 0; i < p; ++i) { sum_xu1_3 += xu1_3[i]; }
+
+    double sum_xu2_3 = 0.0;
+    for (i = 0; i < r; ++i) { sum_xu2_3 += xu2_3[i]; }
+
+    for (i = 0; i < p; ++i) {
+        G[i] = x_u1[i] - (sum_xu1_3 - xu1_3[i]) - sum_xu2_3;
+    }
+
+    for (i = 0; i < r; ++i) {
+        G[p + i] = x_u2[i] - (sum_xu2_3 - xu2_3[i]) - sum_xu1_3;
+    }
+
+}
+
+void SMD10_follower(int p, int q, int r, double *x, double *y, double *f, double *g){
+    int i;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double f1 = sphere(x_u1, p);
+    double f2 = 0.0;
+    double f3 = 0.0;
+
+    for (i = 0; i < q; ++i){
+        f2 += pow(x_l1[i] - 2.0, 2);
+    }
+
+    for (i = 0; i < r; ++i) {
+        f3 += pow( x_u2[i] - tan( x_l2[i] ), 2);
+    }
+
+    f[0] = f1 + f2 + f3;
+
+    double xl1_3[p];
+
+    for (i = 0; i < q; ++i) { xl1_3[i] = pow(x_l1[i], 3); }
+
+    double sum_xl1_3 = 0.0;
+    for (i = 0; i < p; ++i) { sum_xl1_3 += xl1_3[i]; }
+
+
+    for (i = 0; i < q; ++i) {
+        g[i] = x_l1[i] - (sum_xl1_3 - xl1_3[i]);
+    }
+
+
+}
+
+void SMD11_leader(int p, int q, int r, double *x, double *y, double *F, double *G){
+    int i;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double F1 =  sphere(x_u1, p);
+    double F2 = -sphere(x_l1, q);
+    double F3 = 0.0;
+
+    for (i = 0; i < r; ++i) {
+        F3 += pow(x_u2[i], 2) - pow( x_u2[i] - log( x_l2[i] ), 2);
+    }
+
+    F[0] = F1 + F2 + F3;
+
+    double a = 1.0 / sqrt((double) r);
+    
+    for (i = 0; i < r; ++i) {
+        G[i] = x_u2[i] - a - log(x_l2[i]);
+    }
+}
+
+void SMD11_follower(int p, int q, int r, double *x, double *y, double *f, double *g){
+    int i;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double f1 = sphere(x_u1, p);
+    double f2 = sphere(x_l1, q);
+    double f3 = 0.0;
+
+    for (i = 0; i < r; ++i) {
+        f3 += pow( x_u2[i] - log( x_l2[i] ), 2);
+    }
+
+    f[0] = f1 + f2 + f3;
+
+    g[0] = 0.0;
+    for (i = 0; i < r; ++i) {
+        g[0] += pow(x_u2[i] - log(x_l2[i]) ,2);
+    }
+}
+void SMD12_leader(int p, int q, int r, double *x, double *y, double *F, double *G){
+    int i;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double F1 = 0.0;
+    double F2 = sphere(x_l1, q);
+    double F3 = 0.0;
+
+    for (i = 0; i < p; ++i){
+        F1 += pow(x_u1[i] - 2.0, 2);
+    }
+
+    for (i = 0; i < r; ++i) {
+        F3 += pow(x_u2[i] - 2.0, 2) + tan(abs(x_l2[i])) - pow( x_u2[i] - tan( x_l2[i] ), 2);
+    }
+
+    F[0] = F1 + F2 + F3;
+
+    for (i = 0; i < r; ++i) {
+        G[i] = x_u2[i] - tan(x_l2[i]);
+    }
+
+    //////////////////////////////////////////////////////////
+    double xu1_3[p];
+    double xu2_3[p];
+
+    for (i = 0; i < p; ++i) { xu1_3[i] = pow(x_u1[i], 3); }
+    for (i = 0; i < r; ++i) { xu2_3[i] = pow(x_u2[i], 3); }
+
+    double sum_xu1_3 = 0.0;
+    for (i = 0; i < p; ++i) { sum_xu1_3 += xu1_3[i]; }
+
+    double sum_xu2_3 = 0.0;
+    for (i = 0; i < r; ++i) { sum_xu2_3 += xu2_3[i]; }
+    //////////////////////////////////////////////////////////
+
+    for (i = 0; i < p; ++i) {
+        G[r+i] = x_u1[i] - (sum_xu1_3 - xu1_3[i]) - sum_xu2_3;
+    }
+
+    p +=r ;
+    for (i = 0; i < r; ++i) {
+        G[p + i] = x_u2[i] - (sum_xu2_3 - xu2_3[i]) - sum_xu1_3;
+    }
+}
+
+void SMD12_follower(int p, int q, int r, double *x, double *y, double *f, double *g){
+    int i;
+
+    double *x_u1 = x, *x_u2 = &x[p];
+    double *x_l1 = y, *x_l2 = &y[q];
+
+    double f1 = sphere(x_u1, p);
+    double f2 = 0.0;
+    double f3 = 0.0;
+
+    for (i = 0; i < q; ++i){
+        f2 += pow(x_l1[i] - 2.0, 2);
+    }
+
+    for (i = 0; i < r; ++i) {
+        f3 += pow( x_u2[i] - tan( x_l2[i] ), 2);
+    }
+
+    f[0] = f1 + f2 + f3;
+
+    double xl1_3[p];
+
+    for (i = 0; i < q; ++i) { xl1_3[i] = pow(x_l1[i], 3); }
+
+    double sum_xl1_3 = 0.0;
+    for (i = 0; i < p; ++i) { sum_xl1_3 += xl1_3[i]; }
+
+
+    g[0] = f3;
+
+    for (i = 1; i <= q; ++i) {
+        g[i] = x_l1[i] - (sum_xl1_3 - xl1_3[i]);
+    }
+}
+
 void blb18_leader_cop(int N, int D_upper, int D_lower, double *x, double *y, double *F, int id){
     int i, u, l, p, q, r, s;
 
@@ -350,8 +596,6 @@ void blb18_leader_cop(int N, int D_upper, int D_lower, double *x, double *y, dou
         r = D_upper / 2;
         q = D_lower - r;
     }
-
-    printf("%d %d %d %d\n",p,q,r,s );
 
     for (i = 0; i < N; ++i) {
         u = i*D_upper;
