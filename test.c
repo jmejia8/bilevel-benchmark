2aacf15f69729fe86291c2be3f4cb873345b4e09
Jesús Mejía <jesusmejded@gmail.com>
2018-08-23 12:05:34 -0500

    improving the tests

--- test.c
+++ test.c
@@ -16,91 +16,55 @@ double* array(int D){
 }
 
 int test(){
-    int p=3, q=3, r=2, s=0;
-    int D_ul = p + r;
-    int D_ll = q + r;
+    int settings[6];
+    int D_ul = 5,  D_ll = 5;
+    int id;
+
+    blb18_cop_settings(D_ul, D_ll, settings, id);
+
+    int p = settings[0], q = settings[1], r = settings[2], s = settings[3];
+    int lenG, leng;
 
     double *x = array(D_ul);
     double *y = array(D_ll);
-    double F[1], f[1], Fv = 0;
+    double F[1], f[1], f_sum = 0;
+    double *G, *g;
 
-    F[0] = 1000; f[0] = 1000;
-
-    int i = 0;
-
-    ///////////////////////////////////////////////// 
-    ///////////////////////////////////////////////// 
-    blb18_cop_solutions(D_ul, D_ll, x, y, 1);
-
-    SMD1_leader(p, q, r, x, y, F);
-    SMD1_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("1 --> %e\n", F[0]);
-        Fv += F[0];
-    }
-    
-    SMD3_leader(p, q, r, x, y, F);
-    SMD3_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("3 --> %e\n", F[0]);
-        Fv += F[0];
-    }
-
-    SMD4_leader(p, q, r, x, y, F);
-    SMD4_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("4 --> %e\n", F[0]);
-        Fv += F[0];
-    }
-
-    SMD6_leader(p, 1, r, 2, x, y, F);
-    SMD6_follower(p, 1, r, 2, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("6 --> %e\n", F[0]);
-        Fv += F[0];
-    }
-
-    ///////////////////////////////////////////////// 
-    ///////////////////////////////////////////////// 
-    blb18_cop_solutions(D_ul, D_ll, x, y, 2);
-
-    SMD2_leader(p, q, r, x, y, F);
-    SMD2_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("2 --> %e\n", F[0]);
-        Fv += F[0];
-    }
-
-    SMD7_leader(p, q, r, x, y, F);
-    SMD7_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("7 --> %e\n", F[0]);
-        Fv += F[0];
-    }
 
 
     ///////////////////////////////////////////////// 
-    ///////////////////////////////////////////////// 
-    blb18_cop_solutions(D_ul, D_ll, x, y, 5);
-    
-    SMD5_leader(p, q, r, x, y, F);
-    SMD5_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("5 --> %e\n", F[0]);
-        Fv += F[0];
+    /////////////////////////////////////////////////
+    for (id = 1; id <= 12; ++id){
+
+        if (id > 8) {
+            blb18_cop_settings(D_ul, D_ll, settings, id);
+            lenG = settings[4];
+            leng = settings[5];
+            G = array(lenG); g = array(leng);
+
+        }
+
+
+        blb18_cop_solutions(D_ul, D_ll, x, y, id);
+        blb18_leader_cop(1, D_ul, D_ll, x, y, F, G, id);
+        blb18_follower_cop(1, D_ul, D_ll, x, y, f, g, id);
+
+        if (id < 10 && abs(F[0]) + abs(f[0]) > 1e-8) {
+            printf("%d --> F = %e \t f = %e\n", id, F[0], f[0]);
+            f_sum += F[0] + f[0];
+        }
+
+
+         
     }
 
+    // SMD1_leader(p, q, r, x, y, F);
+    // SMD1_follower(p, q, r, x, y, f);
 
-    SMD8_leader(p, q, r, x, y, F);
-    SMD8_follower(p, q, r, x, y, f);
-    if (F[0] + f[0] != 0) {
-        printf("8 --> %e\n", F[0]);
-        Fv += F[0];
-    }
 
-    if (Fv != 0){
+    if (abs(f_sum) > 1e-8){
         printf("Error\n");
-        // exit(0);
+        exit(0);
     }
 
     return 1;
