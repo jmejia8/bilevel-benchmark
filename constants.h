e598215dbfd4464d6f0bfec2161ad21f3c0828c2
Jesus Mejia <jesusmejded@gmail.com>
2019-04-09 11:27:56 -0500

    remove #ifndef

--- constants.h
+++ constants.h
@@ -3,16 +3,9 @@
 // using namespace std;
 
 
-#ifndef PI
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <time.h>
-    #include <math.h>
-
-    #define INF 1.0e99
-    #define EPS 1.0e-14
-    #define E  2.7182818284590452353602874713526625
-    #define PI 3.1415926535897932384626433832795029
-    #include "utils.h"
-#endif
+#include <math.h>
+#define INF 1.0e99
+#define EPS 1.0e-14
+#define E  2.7182818284590452353602874713526625
+#define PI 3.1415926535897932384626433832795029
 
